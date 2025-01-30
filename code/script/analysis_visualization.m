85c09c59f1d51487c8ec1676d28ade23adccfddc
Jona Mac Intyre <64064916+macinj1@users.noreply.github.com>
2025-01-30 11:18:43 +0200

    Update analysis_visualization.m

--- code/script/analysis_visualization.m
+++ code/script/analysis_visualization.m
@@ -5,8 +5,16 @@ function analysis_visualization(BW,centros,indx,c2,count)
 imshow(BW)
 hold on 
 
-plot(centros(indx,1),centros(indx,2),'bo')
+if any(indx)
 
-plot(c2(count,1),c2(count,2),'kx')
+    plot(centros(indx,1),centros(indx,2),'bo')
+
+    if any(count)
+
+        plot(c2(count,1),c2(count,2),'kx')
+
+    end
+
+end
 
 pause(0.05)
